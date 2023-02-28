3fc4c2389dfbcc485a5e38654b1e0f7d130b8418
arusob <sobczyk.arek@gmail.com>
2023-02-28 12:43:42 +0100

    My first changes after your requested changes.
    First changes.

--- anton-and-letters/anton-and-letters.js
+++ anton-and-letters/anton-and-letters.js
@@ -2,24 +2,13 @@ let charaters = "{a, b, c, a, b}"
 let selectDuplicate = ""
 
 function deleteSigns () {
-  NewCharaters = charaters.replace('{','')
-  NewCharaters1 = NewCharaters.replace('}','')
-  NewCharaters2 = NewCharaters1.replaceAll(', ','')
+  newCharaters = charaters.replace('{','')
+  newCharaters1 = newCharaters.replace('}','')
+  newCharaters2 = newCharaters1.replaceAll(', ','')
   }
 deleteSigns()
-console.log(NewCharaters2)
-
-for (i=0; i<=NewCharaters2.length; i++){
-  // console.log(NewCharaters2.charAt(i))
-  if (!selectDuplicate.includes(NewCharaters2.charAt(i))){
-    selectDuplicate += NewCharaters2.charAt(i)
+for (i=0; i<=newCharaters2.length; i++){
+  if (!selectDuplicate.includes(newCharaters2.charAt(i))){
+    selectDuplicate += newCharaters2.charAt(i)
   }
 }
-console.log(selectDuplicate)
-console.log(selectDuplicate.length)
-
-
-
-// {a,b,c,a,b,a,} -> 3
-// Anton wprowadza litery oddzielone przecinkami
-// wydrukować liczbę pjedyńczych liter 
