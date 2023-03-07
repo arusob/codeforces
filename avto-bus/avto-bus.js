ca6c4f24d65a5153e4c2eeabc2ec73071bcd75e7
arusob <sobczyk.arek@gmail.com>
2023-03-07 18:25:36 +0100

    Avto bus. Problem 1679A. Update 7.03./6:25 pm.
    This way of writing the code allows the expected value in the form:
    1. a single number
    2. an array of two numbers

--- avto-bus/avto-bus.js
+++ avto-bus/avto-bus.js
@@ -3,15 +3,14 @@
 var wheels = 24;
 var busSixWheels = 6;
 var busFourWheels = 4;
-
 function checkNumber(wheels) {
   if (wheels % busSixWheels === 0 && wheels % busFourWheels !== 0) {
     return numDivisibleBySix(wheels);
   } else if (wheels % busFourWheels === 0 && wheels % busSixWheels !== 0) {
     return divisibleByFour;
   } else if (wheels % busSixWheels === 0 && wheels % busFourWheels === 0) {
-    var numDeivisbleBySicAndFour = [divisibleBySix, divisibleByFour];
-    return numDeivisbleBySicAndFour;
+    var numDeivisbleBySixAndFour = [divisibleBySix, divisibleByFour];
+    return numDeivisbleBySixAndFour;
   } else if (wheels % 4 !== 0 && wheels % 6 !== 0) {
     return -1;
   }
@@ -27,4 +26,3 @@ function numDivisibleByFour(wheels) {
 }
 var divisibleByFour = numDivisibleByFour(wheels);
 var result = checkNumber(wheels);
-console.log(result);
