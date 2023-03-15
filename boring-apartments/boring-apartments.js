dc7f0f62de6c5f09fc8a29e10f9b2909db252ae1
arusob <sobczyk.arek@gmail.com>
2023-03-15 13:23:18 +0100

    Boring Apartments. Problem 1433A. First improvment.
    Chnge 'var' to 'let'.  Unification name of variables.

--- boring-apartments/boring-apartments.js
+++ boring-apartments/boring-apartments.js
@@ -1,33 +1,34 @@
 // https://codeforces.com/problemset/problem/1433/A
 
-let number = 777;
+var number = 777;
+
 function countBoringNumbers(number) {
-  //console.log("sprawdzam");
-  var array = [];
+  let arrayOfBoringNumbers = [];
   for (let i = 1; i <= 9; i++) {
-    var num = i;
-    var sign = num.toString();
-    var result = "";
+    let num = i;
+    let sign = num.toString();
+    let result = "";
     for (let j = 0; j <= 3; j++) {
       result += sign;
       if (result * 1 === number) {
-        array.push(result);
+        arrayOfBoringNumbers.push(result);
         break;
       }
-      array.push(result);
+      arrayOfBoringNumbers.push(result);
     }
     if (result * 1 === number) {
       break;
     }
   }
-  return array;
+  return arrayOfBoringNumbers;
 }
-var arrayOfBoringNumbers = countBoringNumbers(number);
-function countSingleCharacters(arrayOfBoringNumbers) {
-  var longString = "";
+
+function countSingleCharacters(number) {
+  let arrayOfBoringNumbers = countBoringNumbers(number);
+  let longString = "";
   for (let i = 0; i <= arrayOfBoringNumbers.length - 1; i++) {
     longString += arrayOfBoringNumbers[i];
   }
   return longString.length;
 }
-var numOfClicks = countSingleCharacters(arrayOfBoringNumbers);
+var numOfClicks = countSingleCharacters(number);
