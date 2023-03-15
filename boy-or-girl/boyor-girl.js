2c1c7480dba1984ce41c308d891f84f21f1bb807
arusob <sobczyk.arek@gmail.com>
2023-03-15 13:09:41 +0100

    Boy or Girl. Problem 236A. Delete else.
    Delete 'else'.

--- boy-or-girl/boyor-girl.js
+++ boy-or-girl/boyor-girl.js
@@ -1,18 +1,17 @@
 // https://codeforces.com/problemset/problem/236/A
 
-let username = "wjmzbmr";
+let username = "xiaodao";
 
 function checkWhoItIs(username) {
   let addCharaters = "";
-  for (i = 0; i <= username.length; i++) {
+  for (let i = 0; i <= username.length; i++) {
     if (!addCharaters.includes(username.charAt(i))) {
       addCharaters += username.charAt(i);
     }
   }
   if (addCharaters.length % 2 === 0) {
     return "CHAT WITH HER!";
-  } else {
-    return "IGNORE HIM!";
   }
+  return "IGNORE HIM!";
 }
 var boyOrGirl = checkWhoItIs(username);
