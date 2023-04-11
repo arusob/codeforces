ab7337a48799086a2c4239d01527ad49a1afe521
arusob <sobczyk.arek@gmail.com>
2023-04-11 12:41:41 +0200

    Lucky Sum of Digits. Problem 109A. First change.
    My change after yours comments.

--- lucky-sum-of-digits/lucky-sum-of-digits.js
+++ lucky-sum-of-digits/lucky-sum-of-digits.js
@@ -4,52 +4,43 @@ var input = 11;
 
 function calculateTheTask(input) {
   if (input % 7 === 0) {
-    let result = devideBySeven(input);
-    return result;
+    return devideBySeven(input);
   }
   if (input % 4 === 0) {
-    let result = devideByFour(input);
-    return result;
-  } else {
-    let result = numberOfSevenAndFour(input);
-    return result;
-  }
+    return devideByFour(input);
+  } 
+  return numberOfSevenAndFour(input);
 }
 
 function devideBySeven(input) {
   let resultFromDevideBySeven = input / 7;
-  let result = "7".repeat(resultFromDevideBySeven);
-  return result;
+  return "7".repeat(resultFromDevideBySeven);
 }
 
 function devideByFour(input) {
   let resultFromDevideByFour = input / 4;
-  let result = "4".repeat(resultFromDevideByFour);
-  return result;
+  return "4".repeat(resultFromDevideByFour);
 }
 
 function numberOfSevenAndFour(input) {
-  let resultString = "";
+  let fourSevenString = "";
   var i = 0;
   do {
     let sum = i + 7;
     let rest = input - 7;
     input = input - 7;
-    resultString = resultString + sum;
+    fourSevenString += sum;
     if (rest % 4 === 0 && rest > 0) {
       let howMuchFours = rest / 4;
-      let resultFourString = "4".repeat(howMuchFours);
-      let result = resultString + resultFourString;
-      let arrResult = result.split("").sort();
-      let resultLast = arrResult.join("");
-      return resultLast;
+      let sequenceOfFours = "4".repeat(howMuchFours);
+      let result = fourSevenString + sequenceOfFours;
+      return result.split("").sort().join("");
     }
     if (rest < 0) {
-      let result = "-1";
-      return result;
+      return "-1";
     }
   } while (input > 0);
-  return result;
+
 }
 
 var output = calculateTheTask(input);
