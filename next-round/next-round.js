2cf059a68ecce8027f9bc97ec4d04f1524ee17bb
arusob <sobczyk.arek@gmail.com>
2023-05-05 10:14:14 +0200

    Next round. Next round. Problem 158A. First change
    Change after comments.

--- next-round/next-round.js
+++ next-round/next-round.js
@@ -3,23 +3,23 @@
 var qualification = "8 5";
 var participants = "10 9 8 7 7 7 5 5";
 
-function makeArr (arguments) {
-  let argumentsArray = arguments.split(" ")
-  argumentsArray.forEach(function(el,i,arr) { arr[i] = el * 1;})
-  return argumentsArray
+function makeArr(arguments) {
+  let argumentsArray = arguments.split(" ");
+  argumentsArray.forEach((el, i, arr) => (arr[i] = el * 1));
+  return argumentsArray;
 }
 
-var arrQuali = makeArr (qualification)
-var arrParti = makeArr (participants)
+var arrQuali = makeArr(qualification);
+var arrParti = makeArr(participants);
 
 function analyzeParti(arrQuali, arrPartiNums) {
-  let advanceNextRound = []
-  for(let i=0; i<=arrPartiNums.length-1; i++) {
-      if(arrPartiNums[i] > arrQuali[1]) {
-        advanceNextRound.push(arrPartiNums[i])
-      }
+  let advanceNextRound = [];
+  for (let i = 0; i <= arrPartiNums.length - 1; i++) {
+    if (arrPartiNums[i] > arrQuali[1]) {
+      advanceNextRound.push(arrPartiNums[i]);
+    }
   }
-  return advanceNextRound.length
+  return advanceNextRound.length;
 }
 
-var output = analyzeParti(arrQuali, arrParti)
+var output = analyzeParti(arrQuali, arrParti);
